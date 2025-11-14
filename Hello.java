5426f6295cb40f8ad1f4c5fcfee9d43fde7cdac8
dnagaraju031 <46042454+dnagaraju031@users.noreply.github.com>
2025-11-14 08:46:59 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,18 @@
-public class Hello{
-      public static void main(String[] args) {
-          for(int i=1;i<=10;i++){
-              System.out.println("hello chinna ..."+i);
-                    }
-           }
-}
+version: '3'
+services:
+  php:
+    image: php:8.2-fpm
+    container_name: php
+    volumes:
+      - ./project:/var/www/html
+
+  nginx:
+    image: nginx:latest
+    container_name: nginx
+    ports:
+      - "80:80"
+    volumes:
+      - ./project:/var/www/html
+      - ./nginx.conf:/etc/nginx/conf.d/default.conf
+    depends_on:
+      - php
